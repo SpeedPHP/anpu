2534c4065b3cc6e24cc5ffb94bfe62424e3417f1
speedphp <jake@speedphp.com>
2024-04-21 22:12:13 +0800

    new zgame

---
+++ card/assets/script/Game.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, executionOrder } = _decorator;
+
+@ccclass('Game')
+@executionOrder(-1)
+export class Game extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
